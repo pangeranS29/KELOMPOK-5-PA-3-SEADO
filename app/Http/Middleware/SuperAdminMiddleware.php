4f88a6af0fd063a3cac0530dc7dc9dc2432778ee
Pangeran <pangeransilaen1417@gmail.com>
2025-06-04 16:53:29 +0700

    ubah lagi ke normal

--- app/Http/Middleware/SuperAdminMiddleware.php
+++ app/Http/Middleware/SuperAdminMiddleware.php
@@ -11,7 +11,7 @@ class SuperAdminMiddleware
 {
     public function handle(Request $request, Closure $next): Response
     {
-        if (!auth()->check() || !auth()->user()->isAdmin()) {
+        if (!auth()->check() || !auth()->user()->isSuperAdmin()) {
             abort(403, 'Hanya Super Admin Yang Dapat Membuka Halaman Ini.');
         }
 
